fe6e98cac5d28a7b7d924ed5033a6a8499a121c6
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-13 21:53:16 +0530

    Adding Bootstrap 4 to the project

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -5,7 +5,7 @@ class Dashboard extends Component{
     render() {
         return (
             <div>
-                <h1>Welcome To Dashboard</h1>
+                <h1 className="alert alert-warning">Welcome To Dashboard</h1>
                 <ProjectItem/>
             </div>
         )
