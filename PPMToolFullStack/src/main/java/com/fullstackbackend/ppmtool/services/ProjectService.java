ecc2e902f3ad8e875f0fe388a069f92514ce87de
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-13 16:10:19 +0530

    Project Object validation for identifier and custom exception

--- PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.fullstackbackend.ppmtool.services;
 
 import com.fullstackbackend.ppmtool.domain.Project;
+import com.fullstackbackend.ppmtool.exceptions.ProjectIdException;
 import com.fullstackbackend.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,15 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
 
-        return projectRepository.save(project);
+
+    public Project saveOrUpdateProject(Project project){
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID: '"+project.getProjectIdentifier().toUpperCase()+"' already exist");
+        }
+
     }
 }
