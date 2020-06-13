0713648debe4997d3307480e9c38c998253b074a
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-13 21:43:02 +0530

    Project and Header Component base structure

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 class App extends Component {
   render() {
     return (
         <div className="App">
+          <Header/>
           <Dashboard/>
-          <Dashboard/>
-          <Dashboard/>
+          
         </div>
     );
   }
