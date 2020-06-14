b9a867f0f850cb85a406b830be2090cc988db66f
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-14 14:31:01 +0530

    Adding cross origin

--- PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/web/ProjectController.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProjectController {
     @Autowired
     private ProjectService projectService;
