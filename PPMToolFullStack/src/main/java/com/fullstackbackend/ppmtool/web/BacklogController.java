8ea09c3356802b6db8eecd57cd546e48212f5dc8
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-16 00:00:53 +0530

    Persist Project Task

---
+++ PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/web/BacklogController.java
@@ -0,0 +1,35 @@
+package com.fullstackbackend.ppmtool.web;
+
+import com.fullstackbackend.ppmtool.domain.ProjectTask;
+import com.fullstackbackend.ppmtool.services.MapValidationErrorService;
+import com.fullstackbackend.ppmtool.services.ProjectTaskService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addProjectTaskToBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                                     BindingResult result, @PathVariable String backlog_id){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null){
+            return errorMap;
+        }
+        ProjectTask pTask = projectTaskService.addProjectTask(backlog_id,projectTask);
+        return new ResponseEntity<>(pTask, HttpStatus.CREATED);
+    }
+}
