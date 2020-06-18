9031a3b4a47c73d28d833cefc19fae091ef735f7
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-18 15:44:05 +0530

    User Registration Part 1

--- PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/services/UserService.java
+++ PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/services/UserService.java
@@ -1,7 +1,9 @@
 package com.fullstackbackend.ppmtool.services;
 
+import com.fullstackbackend.ppmtool.domain.User;
 import com.fullstackbackend.ppmtool.repositories.UserReposiroty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +11,12 @@ public class UserService {
     @Autowired
     private UserReposiroty userReposiroty;
 
-    
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser){
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        return userReposiroty.save(newUser);
+    }
+
 }
