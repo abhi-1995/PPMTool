7f65bcd5fa9329f921feb92e1a4d005088597522
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-20 16:31:08 +0530

    Find,Update,Delete Project Task with security

--- PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/web/BacklogController.java
+++ PPMToolFullStack/src/main/java/com/fullstackbackend/ppmtool/web/BacklogController.java
@@ -47,8 +47,8 @@ public class BacklogController {
 
     @Operation(summary = "Get Project Task by Backlog ID and PT Sequence")
     @GetMapping("/{backlog_id}/{pt_id}")
-    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
-        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id,pt_id);
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id,Principal principal){
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id,pt_id,principal.getName());
         return new ResponseEntity<>(projectTask,HttpStatus.OK);
     }
 
@@ -56,20 +56,20 @@ public class BacklogController {
     @PatchMapping("/{backlog_id}/{pt_id}")
     public ResponseEntity<?> updateProjectTask(@Valid @RequestBody ProjectTask projectTask,
                                                BindingResult result, @PathVariable String backlog_id,
-                                               @PathVariable String pt_id){
+                                               @PathVariable String pt_id,Principal principal){
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if (errorMap != null){
             return errorMap;
         }
-        ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask,backlog_id,pt_id);
+        ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask,backlog_id,pt_id,principal.getName());
 
         return new ResponseEntity<>(updatedTask,HttpStatus.OK);
     }
 
     @Operation(summary = "Delete a project task by Backlog ID and Project task sequence ID")
     @DeleteMapping("/{backlog_id}/{pt_id}")
-    public ResponseEntity<?> deleteProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
-        projectTaskService.deletePTByProjectSequence(backlog_id,pt_id);
+    public ResponseEntity<?> deleteProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id, Principal principal){
+        projectTaskService.deletePTByProjectSequence(backlog_id,pt_id,principal.getName());
         return new ResponseEntity<String>("Project Task '"+pt_id+"' was deleted successfully",HttpStatus.OK);
     }
 }
