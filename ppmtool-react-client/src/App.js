352ae445573db61cd9b3038dcf84946037c7c73a
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-21 13:18:05 +0530

    Handle Login logic - 1

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -14,6 +14,25 @@ import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProj
 import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+
+const jwtToken = localStorage.jwtToken
+
+if(jwtToken){
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken
+  });
+
+  const currentTime = Date.now()/100;
+  if(decoded_jwtToken.exp<currentTime){
+    //window.location.href = "/";
+  }
+}
 
 
 class App extends Component {
