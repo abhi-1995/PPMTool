6a02f31efe76114abc4ce35473a82a69a2f03c8d
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-21 16:23:37 +0530

    Pre final commit and bug fix

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,11 +5,11 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
-import AddProjcectTask from "./components/ProjectBoard/ProjectTasks/AddProjcectTask";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjcectTask";
 import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
 import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
@@ -17,13 +17,12 @@ import Login from "./components/UserManagement/Login";
 import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from "./actions/types";
-import {logout} from "./actions/securityActions";
+import { logout } from "./actions/securityActions";
 import SecuredRoute from "./securityUtils/SecureRoute";
 
+const jwtToken = localStorage.jwtToken;
 
-const jwtToken = localStorage.jwtToken
-
-if(jwtToken){
+if (jwtToken) {
   setJWTToken(jwtToken);
   const decoded_jwtToken = jwt_decode(jwtToken);
   store.dispatch({
@@ -31,13 +30,12 @@ if(jwtToken){
     payload: decoded_jwtToken
   });
 
-  const currentTime = Date.now()/100;
-  if(decoded_jwtToken.exp<currentTime){
-    store.dispatch(logout())
-    window.location.href="/"
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    store.dispatch(logout());
+    window.location.href = "/";
   }
 }
-
 
 class App extends Component {
   render() {
@@ -45,7 +43,7 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <Header/>
+            <Header />
             {
               //Public Routes
             }
@@ -58,13 +56,29 @@ class App extends Component {
               //Private Routes
             }
             <Switch>
-              <SecuredRoute exact path="/dashboard" component={Dashboard}/>
-              <SecuredRoute exact path="/addProject" component={AddProject}/>
-              <SecuredRoute exact path="/updateProject/:id" component={UpdateProject}/>
-              <SecuredRoute exact path="/projectBoard/:id" component={ProjectBoard}/>
-              <SecuredRoute exact path="/addProjectTask/:id" component={AddProjcectTask}/> 
-              <SecuredRoute exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask}/>
-            </Switch> 
+              <SecuredRoute exact path="/dashboard" component={Dashboard} />
+              <SecuredRoute exact path="/addProject" component={AddProject} />
+              <SecuredRoute
+                exact
+                path="/updateProject/:id"
+                component={UpdateProject}
+              />
+              <SecuredRoute
+                exact
+                path="/projectBoard/:id"
+                component={ProjectBoard}
+              />
+              <SecuredRoute
+                exact
+                path="/addProjectTask/:id"
+                component={AddProjectTask}
+              />
+              <SecuredRoute
+                exact
+                path="/updateProjectTask/:backlog_id/:pt_id"
+                component={UpdateProjectTask}
+              />
+            </Switch>
           </div>
         </Router>
       </Provider>
--- ppmtool-react-client/src/reducers/projectReducer.js
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -12,18 +12,20 @@ export default function(state = initialState, action) {
         ...state,
         projects: action.payload
       };
+
     case GET_PROJECT:
       return {
         ...state,
         project: action.payload
       };
+
     case DELETE_PROJECT:
       return {
         ...state,
         projects: state.projects.filter(
           project => project.projectIdentifier !== action.payload
         )
-      }
+      };
     default:
       return state;
   }
