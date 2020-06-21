348a17625179520ffebede2e7449b88304ed19f5
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-21 21:37:35 +0530

    BUG

--- ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -62,7 +62,7 @@ class ProjectBoard extends Component {
     BoardContent = boardAlgorithm(errors, project_tasks);
 
     return (
-      <div className="container">
+        <div className="container">
         <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
           <i className="fas fa-plus-circle"> Create Project Task</i>
         </Link>
